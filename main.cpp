be012d8569382b766629750a56055e4e56ddf964
Paramvir <ps17cw@brocku.ca>
2021-03-22 03:41:55 -0400

    encode ready

---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <fstream>
+
+void encode(const std::string m){
+
+    std::string msg{std::move(m)};
+
+    std::unordered_map<std::string, int> dict{};
+    int code = 255;
+
+    std::string crnt{};
+    int next{-1};
+
+    system("echo -n \"\" > encoded.txt");
+
+    std::ofstream encoded{"encoded.txt", std::ios_base::app};
+
+    while(next != (int)msg.size()){
+
+        ++next;
+  
+        if(dict.find(std::string{crnt+msg[next]}) != dict.end()){
+            crnt += msg[next];
+        }
+        else{
+            
+            if(crnt.size()==1){
+                encoded << (int)crnt[0] << std::endl;
+            }
+            else if(crnt.size()>1){
+                encoded << dict[crnt] << std::endl;
+            }
+            
+            crnt += msg[next];
+            dict.insert(std::pair<std::string, int>{crnt,  crnt.size()==1? (int)crnt[0] : ++code });
+            crnt = msg[next];
+
+        }
+       
+    }
+
+}
+
+int main(){
+
+    encode("thisisthe");
+
+    return 0;
+}
