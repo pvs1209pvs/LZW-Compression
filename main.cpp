36ad90d8f94f65e45f05ac5e5ae1edd0dd87d44d
Paramvir <ps17cw@brocku.ca>
2021-03-22 06:56:46 -0400

    string trim removed

--- main.cpp
+++ main.cpp
@@ -6,22 +6,6 @@
 #include <algorithm>
 #include <fstream>
 
-const std::string WHITESPACE = " \n\r\t\f\v";
- 
-std::string ltrim(const std::string& s){
-    size_t start = s.find_first_not_of(WHITESPACE);
-    return (start == std::string::npos) ? "" : s.substr(start);
-}
- 
-std::string rtrim(const std::string& s){
-    size_t end = s.find_last_not_of(WHITESPACE);
-    return (end == std::string::npos) ? "" : s.substr(0, end + 1);
-}
- 
-std::string trim(const std::string& s){
-    return rtrim(ltrim(s));
-}
-
 /**
  * Utilizes Lempel-Ziv-Welch algorithm to compress the message.
  * @param input_file Textfile containing the plain text.
